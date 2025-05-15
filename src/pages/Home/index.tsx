92c24be2efe779e59ec720da120acfbd139d1c93
言午 <990171527@qq.com>
2025-05-15 10:55:02 +0800

    feat: 语义化调整

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,13 +4,13 @@ import BannerBg from './images/banner.webp';
 import './index.less';
 export default function Home() {
   return <div className="home-banner-container">
-    <img src={BannerBg} alt="banner" className="home-banner-bg" />
-    <div className="bg-tip-container">
-      <div className="bg-tip-content">
+    <img src={BannerBg} alt="banner" className="home-banner-banner" />
+    <div className="banner-tip-container">
+      <div className="banner-tip-content">
         用全球好产品推动宠物健康标准升级
       </div>
-      <div className="bg-tip-btn-container">
-        <Button className="bg-tip-btn" type="primary">了解更多</Button>
+      <div className="banner-tip-btn-container">
+        <Button className="banner-tip-btn" type="primary">了解更多</Button>
       </div>
     </div>
   </div>;
