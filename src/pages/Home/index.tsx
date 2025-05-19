4b8384c2c07e5482037f9325e49457db13ee0c67
言午 <990171527@qq.com>
2025-05-20 00:35:20 +0800

    feat: 改动展示

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -35,6 +35,24 @@ export default function Home() {
         <div></div>
       </div>
 
+      <div className="brand-about-us-wrapper">
+        <div className="brand-about-us-container">
+          <div className="brand-about-us-title">关于我们</div>
+          <div className="brand-about-us-content">佩蒂成立于1992年，是中国宠物行业首家上市公司，服务全球宠物30余年，改变的是配方和技术，不变的是对品质的坚持。佩蒂帮助全球宠物品牌设计、生产产品，业务覆盖欧洲、美国、澳洲、日韩、东南亚等国家，旗下拥有Health+、Meatyway、Chewnergy等自主品牌。</div>
+          <div className="brand-about-us-btn-container">
+            <Button className="brand-about-us-btn" type="primary">了解更多</Button>
+          </div>
+        </div>
+      </div>
+      <div className="brand-factory-wrapper">
+        <div className="brand-factory-container">
+          <div className="brand-factory-title">工厂介绍</div>
+          <div className="brand-factory-content">佩蒂的生产基地遍布中国、越南、柬埔寨、新西兰等全球多个国家，获评国家高新技术企业、浙江省级高新技术企业研发中心、浙江省省级企业研究院、温州市博士创新工作站，有效专利57个，3个国际发明专利、8个国家发明专利。</div>
+          <div className="brand-factory-btn-container">
+            <Button className="brand-factory-btn" type="primary">了解更多</Button>
+          </div>
+        </div>
+      </div>
     </div>
   </>
 } 
