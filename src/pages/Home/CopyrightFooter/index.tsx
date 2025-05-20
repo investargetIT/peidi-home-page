991d877f6b7c54a31e78cac9eeea238fd04b42d7
言午 <990171527@qq.com>
2025-05-20 17:14:15 +0800

    feat: 组件抽离

---
+++ src/pages/Home/CopyrightFooter/index.tsx
@@ -0,0 +1,11 @@
+import './index.less';
+
+export default function CopyrightFooter() {
+  return (
+    <div className="copyright-footer">
+      <div className="copyright-text">
+        Copyright © 2024 佩蒂 保留所有权利.
+      </div>
+    </div>
+  );
+} 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,6 +7,8 @@ import FactoryIntro from './FactoryIntro';
 import FactoryShowcase from './FactoryShowcase';
 import ProductInnovation from './ProductInnovation';
 import ResearchDevelopment from './ResearchDevelopment';
+import FooterNav from './FooterNav';
+import CopyrightFooter from './CopyrightFooter';
 import './index.less';
 
 export default function Home() {
@@ -20,44 +22,7 @@ export default function Home() {
     <FactoryShowcase />
     <ProductInnovation onLearnMoreClick={() => console.log('Product innovation learn more clicked')} />
     <ResearchDevelopment />
-    <div className="footer-nav-wrapper">
-      <div className="footer-nav-container">
-        <div className="footer-nav-column">
-          <h3 className="footer-nav-title">佩蒂创新</h3>
-          <ul className="footer-nav-links">
-            <li><a href="#">生产基地</a></li>
-            <li><a href="#">生产能力</a></li>
-            <li><a href="#">研发能力</a></li>
-            <li><a href="#">订单流程</a></li>
-            <li><a href="#">质量管理</a></li>
-            <li><a href="#">产品中心</a></li>
-          </ul>
-        </div>
-        <div className="footer-nav-column">
-          <h3 className="footer-nav-title">主菜单</h3>
-          <ul className="footer-nav-links">
-            <li><a href="#">企业文化</a></li>
-            <li><a href="#">发展历程</a></li>
-            <li><a href="#">品牌产品</a></li>
-            <li><a href="#">新闻中心</a></li>
-            <li><a href="#">联系我们</a></li>
-          </ul>
-        </div>
-        <div className="footer-nav-column">
-          <h3 className="footer-nav-title">品牌产品</h3>
-          <ul className="footer-nav-links">
-            <li><a href="#">爵宴</a></li>
-            <li><a href="#">好适嘉</a></li>
-            <li><a href="#">齿能</a></li>
-            <li><a href="#">SmartBones</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-    <div className="copyright-footer">
-      <div className="copyright-text">
-        Copyright © 2024 佩蒂 保留所有权利.
-      </div>
-    </div>
+    <FooterNav />
+    <CopyrightFooter />
   </>
 } 
