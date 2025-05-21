38c84f8e18f7d10a1f17adbb8a4060a9d62d9775
言午 <990171527@qq.com>
2025-05-21 09:49:43 +0800

    feat: 展示调整

---
+++ src/pages/InvestorRelations/Investor/index.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function Investor() {
+  return <div>投资者服务内容</div>;
+} 
---
+++ src/pages/InvestorRelations/Report/index.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function Report() {
+  return <div>定期报告内容</div>;
+} 
