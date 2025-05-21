a8e1c0cfc0913b21fb8d869b53aa54ad13bed389
言午 <990171527@qq.com>
2025-05-21 11:16:08 +0800

    feat: 展示调整

--- src/pages/InvestorRelations/Market/index.tsx
+++ src/pages/InvestorRelations/Market/index.tsx
@@ -1,4 +1,24 @@
 import React from 'react';
+import './index.less';
+import StockInfo from '../components/StockInfo';
+
 export default function Market() {
-  return <div>公司公告内容</div>;
+  return (
+    <div className="market-page">
+      <StockInfo stockName="佩蒂股份" stockCode="300673" />
+      <div className="section-title-area">
+        <div className="section-title-container">
+          <div className="section-title-box">
+            <div className="section-title-underline"></div>
+            <h2 className="section-title">公司公告</h2>
+          </div>
+        </div>
+      </div>
+
+      <div className="market-content">
+        {/* Market content will go here */}
+        <iframe src="https://webapi.cninfo.com.cn/notice.html?api=p_sysapi1091&stype=1&prefix=sysapi&scode=300673&column=F002V,SORTNAME,F001D,F004V" width="100%" height="1000px"></iframe>
+      </div>
+    </div>
+  );
 } 
