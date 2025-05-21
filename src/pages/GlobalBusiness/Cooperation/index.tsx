6e554adb3e695df9f9d69ba568d4399690d1270f
言午 <990171527@qq.com>
2025-05-21 13:40:44 +0800

    feat: 展示调整

---
+++ src/pages/GlobalBusiness/Cooperation/index.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+export default function Cooperation() {
+  return <div>合作内容</div>;
+} 
--- src/pages/GlobalBusiness/index.tsx
+++ src/pages/GlobalBusiness/index.tsx
@@ -1,4 +1,40 @@
-import React from 'react';
-export default function GlobalBusiness() {
-  return <div>全球行业业务内容</div>;
+import { useState } from 'react';
+import { Tabs } from 'antd';
+import type { TabsProps } from 'antd';
+import ODM from './ODM';
+import Cooperation from './Cooperation';
+import SolutionBanner from './images/cooperation.jpg';
+import './index.less';
+
+export default function InvestorRelations() {
+  const [activeTab, setActiveTab] = useState('solution');
+
+  const items: TabsProps['items'] = [
+    {
+      key: 'odm',
+      label: 'ODM',
+      children: <ODM />,
+    },
+    {
+      key: 'cooperation',
+      label: '合作',
+      children: <Cooperation />,
+    },
+
+
+  ];
+
+  return (
+    <div className="investor-relations">
+      <div className="solution-banner">
+        <img src={SolutionBanner} alt="solution-banner" />
+      </div>
+      <Tabs
+        items={items}
+        defaultActiveKey={activeTab}
+        onChange={(key) => setActiveTab(key)}
+        centered
+      />
+    </div>
+  );
 }
