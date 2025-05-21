859f54ad245bd958e6250fa4805392601db6b0c5
言午 <990171527@qq.com>
2025-05-21 12:01:27 +0800

    feat: 展示调整

--- src/pages/InvestorRelations/Investor/index.tsx
+++ src/pages/InvestorRelations/Investor/index.tsx
@@ -1,4 +1,41 @@
 import React from 'react';
+import './index.less';
+import StockInfo from '../components/StockInfo';
+
 export default function Investor() {
-  return <div>投资者服务内容</div>;
+  return (
+    <div className="investor-page">
+      <StockInfo stockName="佩蒂股份" stockCode="300673" />
+
+      <div className="investor-header">
+        <div className="header-content">
+          <h2>投资者联络</h2>
+        </div>
+      </div>
+
+      <div className="investor-contact-container">
+        <div className="contact-table">
+          <div className="contact-row">
+            <div className="contact-label">联系地址</div>
+            <div className="contact-value">浙江省平阳县水头镇工业园区宏东路2号</div>
+          </div>
+
+          <div className="contact-row">
+            <div className="contact-label">电话</div>
+            <div className="contact-value">0577-5818 9955</div>
+          </div>
+
+          <div className="contact-row">
+            <div className="contact-label">传真</div>
+            <div className="contact-value">0577-5818 9966</div>
+          </div>
+
+          <div className="contact-row">
+            <div className="contact-label">电子邮箱</div>
+            <div className="contact-value">cpet@peidibrand.com</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 } 
--- src/pages/InvestorRelations/Report/index.tsx
+++ src/pages/InvestorRelations/Report/index.tsx
@@ -1,4 +1,24 @@
 import React from 'react';
-export default function Report() {
-  return <div>定期报告内容</div>;
+import './index.less';
+import StockInfo from '../components/StockInfo';
+
+export default function Market() {
+  return (
+    <div className="market-page">
+      <StockInfo stockName="佩蒂股份" stockCode="300673" />
+      <div className="section-title-area">
+        <div className="section-title-container">
+          <div className="section-title-box">
+            <div className="section-title-underline"></div>
+            <h2 className="section-title">定期报告</h2>
+          </div>
+        </div>
+      </div>
+
+      <div className="market-content">
+        {/* Market content will go here */}
+        <iframe src="https://webapi.cninfo.com.cn/notice.html?api=p_sysapi1091&stype=1&prefix=sysapi&scode=300673&column=F002V,SORTNAME,F001D,F004V&sortcode=0103" width="100%" height="1000px"></iframe>
+      </div>
+    </div>
+  );
 } 
