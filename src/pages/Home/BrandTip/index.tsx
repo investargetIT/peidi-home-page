fcd06edb1e01ec0408b28e35d9c81e4e5554d662
言午 <990171527@qq.com>
2025-05-28 15:37:08 +0800

    feat: 展示调整

--- src/pages/Home/BrandTip/index.tsx
+++ src/pages/Home/BrandTip/index.tsx
@@ -2,12 +2,12 @@
  * 品牌提示组件
  * 展示公司的主要宠物食品品牌信息和品牌图标
  */
-import MeatIcon from './images/meat.webp';
-import HealthIcon from './images/health.webp';
-import ChewIcon from './images/chew.webp';
-import SmartIcon from './images/smart.webp';
 import './index.less';
 import { Button } from 'antd';
+import OemIcon from './images/oem.jpg';
+import NewsIcon from './images/news.jpg';
+import ChannelIcon from './images/channel.jpg';
+import AboutIcon from './images/about.jpg';
 
 export default function BrandTip() {
   const onLearnMoreClick = () => {
@@ -40,24 +40,24 @@ export default function BrandTip() {
       {/* 品牌图标展示区域 */}
       <div className="brand-tip-icon-wrapper">
         <div className="brand-tip-icon-container">
-          {/* 肉食图标 */}
-          <div className="brand-tip-icon-item">
-            <img src={MeatIcon} alt="meat" className="brand-tip-icon" />
+          {/* OEM&ODM卡片 */}
+          <div className="brand-tip-card">
+            <img src={OemIcon} alt="OEM&ODM" className="brand-tip-card-image" />
           </div>
 
-          {/* 健康图标 */}
-          <div className="brand-tip-icon-item">
-            <img src={HealthIcon} alt="health" className="brand-tip-icon" />
+          {/* 新闻中心卡片 */}
+          <div className="brand-tip-card">
+            <img src={NewsIcon} alt="新闻中心" className="brand-tip-card-image" />
           </div>
 
-          {/* 咀嚼图标 */}
-          <div className="brand-tip-icon-item">
-            <img src={ChewIcon} alt="chew" className="brand-tip-icon" />
+          {/* 购买渠道卡片 */}
+          <div className="brand-tip-card">
+            <img src={ChannelIcon} alt="购买渠道" className="brand-tip-card-image" />
           </div>
 
-          {/* 智能图标 */}
-          <div className="brand-tip-icon-item">
-            <img src={SmartIcon} alt="smart" className="brand-tip-icon" />
+          {/* 关注佩蒂卡片 */}
+          <div className="brand-tip-card">
+            <img src={AboutIcon} alt="关注佩蒂" className="brand-tip-card-image" />
           </div>
         </div>
       </div>
