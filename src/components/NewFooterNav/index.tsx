c58205e51e76e7bc67ff3bc3e303771b51662a38
言午 <990171527@qq.com>
2025-05-28 16:45:16 +0800

    feat: 上下间距调整

--- src/components/NewFooterNav/index.tsx
+++ src/components/NewFooterNav/index.tsx
@@ -3,9 +3,7 @@
  * 展示网站底部的导航链接区域
  */
 import './index.less';
-import { Link } from 'react-router-dom';
 import BrandLogo from './images/banner.png';
-import InfinityLogo from './images/we-chat.jpg';
 import QRCode from './images/we-chat.jpg';
 import WeiboIcon from './images/weibo.jpg';
 
@@ -19,25 +17,25 @@ export default function FooterNav() {
       </div>
 
       {/* Copyright Footer Section */}
-      <div className="copyright-footer">
-        <div className="copyright-divider"></div>
+      <div className="bottom-copyright-section">
+        <div className="bottom-divider"></div>
 
-        <div className="copyright-content">
-          <div className="copyright-info">
+        <div className="bottom-content-container">
+          <div className="bottom-legal-info">
             <p>Copyright © 2018 佩蒂动物营养科技股份有限公司 All Rights Reserved.</p>
-            <p className="license-info">
-              <span className="security-license">浙公网安备 33032602100082号</span>
-              <span className="icp-license">浙ICP备13035411号</span>
+            <p className="bottom-license-text">
+              <span className="bottom-security-license">浙公网安备 33032602100082号</span>
+              <span className="bottom-icp-license">浙ICP备13035411号</span>
               <span>佩蒂动物营养科技股份有限公司 保留所有权利</span>
             </p>
           </div>
 
-          <div className="copyright-contact">
-            <div className="social-qrcode">
-              <img src={QRCode} alt="QR Code" className="qrcode" />
-              <img src={WeiboIcon} alt="Weibo" className="weibo-icon" />
+          <div className="bottom-contact-container">
+            <div className="bottom-social-media">
+              <img src={QRCode} alt="QR Code" className="bottom-qrcode" />
+              <img src={WeiboIcon} alt="Weibo" className="bottom-weibo" />
             </div>
-            <div className="contact-phone">400-836-1508</div>
+            <div className="bottom-phone-number">400-836-1508</div>
           </div>
         </div>
       </div>
