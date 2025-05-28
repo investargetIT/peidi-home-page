3a9a7d7c2642866203e51b6a84f42af008a18cb4
言午 <990171527@qq.com>
2025-05-28 15:40:23 +0800

    feat:展示调整

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -16,38 +16,42 @@ import CopyrightFooter from '@/components/CopyrightFooter';
 import './index.less';
 
 export default function Home() {
-  return <>
-    {/* 首页顶部banner区域 */}
-    <HomeBanner onLearnMoreClick={() => console.log('Learn more clicked')} />
+  return (
+    <>
+      {/* 首页顶部banner区域 */}
+      <HomeBanner onLearnMoreClick={() => console.log('Learn more clicked')} />
 
-    {/* 品牌提示与图标展示区域 */}
-    <BrandTip />
+      {/* 品牌提示与图标展示区域 */}
+      <BrandTip />
 
-    {/* 关于我们介绍区域 */}
-    <AboutUs onLearnMoreClick={() => console.log('About us learn more clicked')} />
+      {/* 关于我们介绍区域 */}
+      {/* <AboutUs onLearnMoreClick={() => console.log('About us learn more clicked')} /> */}
 
-    {/* 公司文化展示区域 */}
-    <CompanyCulture />
+      {/* 公司文化展示区域 */}
+      {/* <CompanyCulture /> */}
 
-    {/* 文化理念图标展示区域 */}
-    <CultureIcons />
+      {/* 文化理念图标展示区域 */}
+      {/* <CultureIcons /> */}
 
-    {/* 工厂介绍区域 */}
-    <FactoryIntro onLearnMoreClick={() => console.log('Factory intro learn more clicked')} />
+      {/* 工厂介绍区域 */}
+      {/* <FactoryIntro onLearnMoreClick={() => console.log('Factory intro learn more clicked')} /> */}
 
-    {/* 工厂展示轮播区域 */}
-    <FactoryShowcase />
+      {/* 工厂展示轮播区域 */}
+      <FactoryShowcase />
 
-    {/* 产品创新区域 */}
-    <ProductInnovation onLearnMoreClick={() => console.log('Product innovation learn more clicked')} />
+      {/* 产品创新区域 */}
+      {/* <ProductInnovation
+        onLearnMoreClick={() => console.log('Product innovation learn more clicked')}
+      /> */}
 
-    {/* 研发能力展示区域 */}
-    <ResearchDevelopment />
+      {/* 研发能力展示区域 */}
+      {/* <ResearchDevelopment /> */}
 
-    {/* 页脚导航区域 */}
-    <FooterNav />
+      {/* 页脚导航区域 */}
+      <FooterNav />
 
-    {/* 版权信息区域 */}
-    <CopyrightFooter />
-  </>
-} 
+      {/* 版权信息区域 */}
+      <CopyrightFooter />
+    </>
+  );
+}
