7b286e4f165a408ac4e66dbf48bccc2dfa116ada
言午 <990171527@qq.com>
2025-06-05 15:07:39 +0800

    feat: 逻辑接入

--- src/components/NewFooterNav/index.tsx
+++ src/components/NewFooterNav/index.tsx
@@ -6,8 +6,49 @@ import './index.less';
 import BrandLogo from './images/banner.png';
 import QRCode from './images/we-chat.jpg';
 import WeiboIcon from './images/weibo.jpg';
+import WeChatPet from './images/pet-wechat.jpg';
+import JueYanRed from './images/xiaohongshu-jueyan.png';
+import HaoshijiaRed from './images/xiaohongshu-haohsijia.png';
+import TianMaoPet from './images/tianmao-peidi.jpg';
+import JingdongPet from './images/jingdong-jueyan.jpg';
+import DouyinPet from './images/douyin-jueyan.png';
 
 export default function FooterNav() {
+  const qrList = [
+    {
+      src: QRCode,
+      alt: '佩蒂股份公众号',
+    },
+    {
+      src: WeiboIcon,
+      alt: '佩蒂星球微博',
+    },
+    {
+      src: WeChatPet,
+      alt: '佩蒂宠物公众号',
+    },
+    {
+      src: JueYanRed,
+      alt: '小红书爵宴官方账号',
+    },
+    {
+      src: HaoshijiaRed,
+      alt: '小红书好适嘉官方账号',
+    },
+    {
+      src: TianMaoPet,
+      alt: '天猫佩蒂旗舰店',
+    },
+    {
+      src: JingdongPet,
+      alt: '京东爵宴旗舰店',
+    },
+    {
+      src: DouyinPet,
+      alt: '抖音爵宴旗舰店',
+    },
+  ];
+
   return (
     <>
       <div className="footer-nav-wrapper">
